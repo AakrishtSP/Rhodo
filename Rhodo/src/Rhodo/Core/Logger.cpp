5e5345500ab15b6c46a3d50de563a576842229ad
ASP <akrisht62@gmail.com>
2025-01-21 22:21:00 +0545

    bland comit

--- Rhodo/src/Rhodo/Core/Logger.cpp
+++ Rhodo/src/Rhodo/Core/Logger.cpp
@@ -15,10 +15,10 @@ namespace Rhodo
 
 	void Logger::Init()
 	{
-		s_CoreLogger = spdlog::stdout_color_mt("RHODO");
+		s_CoreLogger = spdlog::create<spdlog::sinks::stdout_color_sink_mt>("RHODO");
 		s_CoreLogger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] %^[%l] [%t] %v%$");
 		s_CoreLogger->set_level(spdlog::level::trace);
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger = spdlog::create<spdlog::sinks::stdout_color_sink_mt>("APP");
 		s_ClientLogger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] %^[%l] [%t] [%s:%#] %v%$");
 		s_ClientLogger->set_level(spdlog::level::trace);
 	}
